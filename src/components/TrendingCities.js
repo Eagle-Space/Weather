07344bb3e0ac10f17555a0c9a94496aeceb9e60d
Eagle-Space <124315051+Eagle-Space@users.noreply.github.com>
2023-05-11 16:59:31 -0600

    Update TrendingCities.js

--- src/components/TrendingCities.js
+++ src/components/TrendingCities.js
@@ -1,11 +1,10 @@
-// components/TrendingCities.js
 import React, { useState, useEffect } from 'react';
 
 function TrendingCities() {
   const [trending, setTrending] = useState([]);
 
   useEffect(() => {
-    // Retrieve trending cities from local storage
+    // Fetch trending cities from local storage
     const storedTrending = localStorage.getItem('trending');
     if (storedTrending) {
       setTrending(JSON.parse(storedTrending));
