68452fc587bd3e50743f3905d2c1400237c6f8fc
Eagle-Space <124315051+Eagle-Space@users.noreply.github.com>
2023-05-11 16:58:59 -0600

    Update SearchHistory.js

--- src/components/SearchHistory.js
+++ src/components/SearchHistory.js
@@ -1,13 +1,13 @@
-// components/SearchHistory.js
 import React, { useState, useEffect } from 'react';
 
 function SearchHistory() {
   const [history, setHistory] = useState([]);
 
   useEffect(() => {
+    // Fetch search history from local storage when component loads
     const storedHistory = localStorage.getItem('history');
     if (storedHistory) {
-      setHistory(JSON.parse(storedHistory));
+      setHistory(JSON.parse(storedHistory)); // Set the search history from local storage in state
     }
   }, []);
 
@@ -16,7 +16,7 @@ function SearchHistory() {
       <h2>Search History</h2>
       <ul>
         {history.map(city => (
-          <li key={city}>{city}</li>
+          <li key={city}>{city}</li> // Display each searched city as a list item
         ))}
       </ul>
     </div>
