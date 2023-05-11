17bfa8a5d8c6065e40d617680a54c60a35c680b4
Eagle-Space <124315051+Eagle-Space@users.noreply.github.com>
2023-05-11 16:57:48 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import TrendingCities from './TrendingCities';
+import FavoriteCities from './FavoriteCities';
+import SearchHistory from './SearchHistory';
 
 function Home() {
   return (
     <div>
       <h2>Home</h2>
-      <div className="main-content">
-        <p>Welcome to the Weather App!</p>
-        <TrendingCities />
-
-      </div>
-     
-      </div>
+      <FavoriteCities />
+      <TrendingCities />
+      <SearchHistory />
+    </div>
   );
 }
 
